22c17a0aa8d91464f325863e61b3d48a92495ae4
sedazam <sedazam@gmail.com>
2025-07-29 14:59:22 +0100

    Add episode details rendering in setup function

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function setup() {
 const rootElem = document.getElementById("root");
 rootElem.textContent = "Got ${episodeList.length} episode(s)";
 
+episodeList.forEach((episode) => {
+  const episodeCode = `S${season}E${number}`;
+  episodeDiv.textContent = `${episodeCode} - ${episode.name}`;
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
